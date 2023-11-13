91c15086439371f41ace5867b3ed0ec4a993d30e
greatYear <ywzhaiqi@gmail.com>
2023-11-13 18:31:23 +0800

    Add 播放列表

--- src/pages/home/previews/video_box.tsx
+++ src/pages/home/previews/video_box.tsx
@@ -10,16 +10,22 @@ import {
 import { For, JSXElement } from "solid-js"
 import { useRouter, useLink, useT } from "~/hooks"
 import { objStore } from "~/store"
-import { ObjType } from "~/types"
+import { ObjType, StoreObj } from "~/types"
 import { convertURL } from "~/utils"
 import Artplayer from "artplayer"
 import { SelectWrapper } from "~/components"
 
 Artplayer.PLAYBACK_RATE = [0.5, 0.75, 1, 1.25, 1.5, 2, 3, 4]
 
-export const players: { icon: string; name: string; scheme: string }[] = [
+export const players: {
+  icon: string
+  name: string
+  scheme: string
+  pls?: boolean
+}[] = [
   { icon: "iina", name: "IINA", scheme: "iina://weblink?url=$durl" },
   { icon: "potplayer", name: "PotPlayer", scheme: "potplayer://$durl" },
+  { icon: "potplayer", name: "播放列表", pls: true, scheme: "" },
   { icon: "vlc", name: "VLC", scheme: "vlc://$durl" },
   { icon: "nplayer", name: "nPlayer", scheme: "nplayer-$durl" },
   {
@@ -41,12 +47,21 @@ export const players: { icon: string; name: string; scheme: string }[] = [
   },
 ]
 
+function createPlsLink(videos: StoreObj[], rawLink: Function): string {
+  const text = `[playlist]\nNumberOfEntries=${videos.length}
+${videos.map((v, i) => `File${i + 1}=${rawLink(v, true)}`).join("\n")}`
+
+  const textBlob = new Blob([text], { type: "text/plain;charset=utf-8" })
+  const url = URL.createObjectURL(textBlob)
+  return url
+}
+
 export const VideoBox = (props: {
   children: JSXElement
   onAutoNextChange: (v: boolean) => void
 }) => {
   const { replace } = useRouter()
-  const { currentObjLink } = useLink()
+  const { currentObjLink, rawLink } = useLink()
   let videos = objStore.objs.filter((obj) => obj.type === ObjType.VIDEO)
   if (videos.length === 0) {
     videos = [objStore.obj]
@@ -91,11 +106,16 @@ export const VideoBox = (props: {
               <Tooltip placement="top" withArrow label={item.name}>
                 <Anchor
                   // external
-                  href={convertURL(item.scheme, {
-                    raw_url: objStore.raw_url,
-                    name: objStore.obj.name,
-                    d_url: currentObjLink(true),
-                  })}
+                  href={
+                    item.pls
+                      ? createPlsLink(videos, rawLink)
+                      : convertURL(item.scheme, {
+                          raw_url: objStore.raw_url,
+                          name: objStore.obj.name,
+                          d_url: currentObjLink(true),
+                        })
+                  }
+                  download={item.pls ? "alist播放列表.pls" : ""}
                 >
                   <Image
                     m="0 auto"
