790656f41cc0a4830665d7007199b44a5da19fc7
greatYear <ywzhaiqi@gmail.com>
2023-12-13 16:13:38 +0800

    aliyun_video Add next button

--- src/pages/home/previews/aliyun_video.tsx
+++ src/pages/home/previews/aliyun_video.tsx
@@ -204,6 +204,21 @@ const Preview = () => {
           }
         }
       })
+      // add next button
+      const index = videos.findIndex((f) => f.name === objStore.obj.name)
+      const nextIndex = index + 1
+      if (nextIndex < videos.length - 1) {
+        player.controls.add({
+          name: "next",
+          index: 1,
+          position: "right",
+          html: "<span>下一个</span>",
+          tooltip: "播放下一个视频",
+          click: () => {
+            replace(videos[nextIndex].name)
+          },
+        })
+      }
     })
   })
   onCleanup(() => {
